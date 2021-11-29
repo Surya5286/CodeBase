deb47287ab33d8ace09ff069551debc947279292
Surya5286 <annakalyan286@gmail.com>
2021-11-29 21:20:34 +0530

    Updated the Pattern class methods with the required output format

--- src/com/codingPractice/Pattern/PatternClass.java
+++ src/com/codingPractice/Pattern/PatternClass.java
@@ -16,6 +16,15 @@ public class PatternClass {
         getPattern6(n);
     }
 
+    /* Print an number pyramid
+                1
+              2 1 2
+            3 2 1 2 3
+          4 3 2 1 2 3 4
+        5 4 3 2 1 2 3 4 5
+
+    */
+
     public static void getPattern6(int n){
         for (int row = 1; row <= n; row++) {
 
@@ -34,6 +43,20 @@ public class PatternClass {
         }
     }
 
+    /*  Print a diamond shaped * pattern
+
+         *
+        * *
+       * * *
+      * * * *
+     * * * * *
+      * * * *
+       * * *
+        * *
+         *
+
+     */
+
     public static void getPattern5(int n) {
         for (int i = 0; i < 2*n; i++) {
             int totalColInRows = i > n ? 2 * n - i: i;
@@ -51,6 +74,20 @@ public class PatternClass {
         }
     }
 
+    /*  Print an incremental & decremental triangle
+
+     *
+     * *
+     * * *
+     * * * *
+     * * * * *
+     * * * *
+     * * *
+     * *
+     *
+
+     */
+
     public static void getPattern4(int n) {
         for (int i = 0; i < 2*n; i++) {
             int totalColumnsInRow = i>n? 2*n-i:i;
@@ -61,6 +98,17 @@ public class PatternClass {
         }
     }
 
+    /* Print an inverted Right angled Triangle
+
+     * * * * *
+     * * * *
+     * * *
+     * *
+     *
+
+
+     */
+
     public static void getPattern3(int n) {
         for (int i = 1; i <= n ; i++) {
             for( int j = 1; j <= n -i + 1; j++){
@@ -70,6 +118,15 @@ public class PatternClass {
         }
     }
 
+    /* Print a right angle triangle
+
+     *
+     * *
+     * * *
+     * * * *
+     * * * * *
+
+     */
     public static void getPattern2(int n) {
         for (int i = 1; i <= n ; i++) {
             for( int j = 1; j<=i; j++){
@@ -79,6 +136,15 @@ public class PatternClass {
         }
     }
 
+
+    /*  Print a Square pattern
+     * * * * *
+     * * * * *
+     * * * * *
+     * * * * *
+     * * * * *
+     */
+
     public static void getPattern1(int n) {
         for (int i = 1; i <= n; i++) {
             for(int j = 1; j<=n; j++){
@@ -87,5 +153,6 @@ public class PatternClass {
             System.out.println();
         }
     }
+
 
 }
